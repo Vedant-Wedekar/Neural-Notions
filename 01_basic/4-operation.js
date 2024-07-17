4eb8404dae4ccfac0e59cd91ae36ca63e29a17e0
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-07-17 05:58:58 +0000

    opeartion on string

---
+++ 01_basic/4-operation.js
@@ -0,0 +1,50 @@
+//negation by sign 
+
+let pos = 13 
+let neg = -pos
+
+console.log(neg);
+
+//for power
+// output will be  
+// 2 * * 3 = 8 
+
+for(let i = 0 ;i<=10;i++){
+    console.log(2**i);
+}
+
+//add two variable 
+
+let str_1 = 1
+let str_2 = 2
+console.log(str_1 + str_2);
+
+// add two string 
+console.log("vedant" + " wedekar")
+
+// num like string 
+
+console.log("9."+36);
+
+// but listine its javascript my brooooo
+
+console.log(2+2+"3");// the output will be 43
+//but why let me explain 
+// when you add two variable 
+
+//case 1 = if they are two string they will get attached 
+// dry code (console.log("vedant" + " wedekar"))
+
+// case 2 = if the first is string and second is num the they got attached
+// dry code (console.log("9."+36);)
+
+//case 3 = if first is num or numbers they firstly got addition then they got attached with the string
+// dry code (console.log(2+2+"3");)
+
+
+
+
+
+
+
+
