05c1b701bc4a38c017ca113692e0d5d7b8ee5c64
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-07-17 05:16:17 +0000

    data-conversion

---
+++ 01_basic/3-data-conversion.js
@@ -0,0 +1,29 @@
+let name = "vedant"
+
+let str_into_num = Number(name)
+
+console.log(str_into_num);
+
+// boolean 
+
+let op = true
+let val = Number(op)
+console.log(val);
+
+let opp = false
+let vale = Number(opp)
+console.log(vale);
+
+//boolean into string 
+
+
+let pa = 1
+let ya = String(pa)
+console.log(ya);
+console.log(typeof ya);
+
+
+let pr = 0
+let re = String(pr)
+console.log(re);
+console.log(typeof re);
