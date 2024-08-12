3f6ef248c18890c465af30f6925328cef5fb70cc
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-08-12 21:29:09 +0000

    minute basic structure

---
+++ 01_basic/11-object.js
@@ -0,0 +1,18 @@
+// singleton
+// create.object()
+
+//object literals
+
+//a normal declaretion an object 
+
+const jsuser = {
+    name:`vedant`,
+    age: 18,
+    loc : `nagpur`,
+    "email":`xyz@gmail.com`,
+    favpeople:[`m`,`p`,`a`,`n`,`p`,`m`]
+
+}
+
+console.log(jsuser.age)
+console.log(jsuser["email"])
