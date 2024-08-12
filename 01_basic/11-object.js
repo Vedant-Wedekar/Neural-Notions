b107822370ddd27d62141a27022dbef3d5ce3233
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-08-12 22:35:26 +0000

    symbols in js  object

--- 01_basic/11-object.js
+++ 01_basic/11-object.js
@@ -5,14 +5,26 @@
 
 //a normal declaretion an object 
 
+
+//symbol access
+const mysym = Symbol("ogsymbol")
 const jsuser = {
+    mysym : `fakesymbol`,
+    [mysym] : `fakesymbol`,
     name:`vedant`,
     age: 18,
     loc : `nagpur`,
     "email":`xyz@gmail.com`,
     favpeople:[`m`,`p`,`a`,`n`,`p`,`m`]
 
-}
+    //symbol 
 
+}
+console.log(jsuser.mysym)
+console.log(jsuser[mysym])
+console.log(typeof jsuser.mysym)
+console.log(typeof jsuser[mysym])
 console.log(jsuser.age)
 console.log(jsuser["email"])
+
+
