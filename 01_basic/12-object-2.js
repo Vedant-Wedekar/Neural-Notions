ad6d84931e872020f155a7221b95bd5ee0ac5985
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-08-15 04:39:27 +0000

    just in 10 min of object in object objectt

---
+++ 01_basic/12-object-2.js
@@ -0,0 +1,21 @@
+const tinderuser = new Object();
+console.log(tinderuser);
+
+//including the attribute  inside the object 
+tinderuser.name = 'vedant wedekar'
+tinderuser.gender = `male `
+
+console.log(tinderuser);
+
+//object  inside the objecbbb 
+const obj1 = {
+    email :`vedantwedekar@gmail.com`,
+    username: {
+        name :{
+        fullname:`vedant wedekar`,
+        last_name : `wedekar `,
+        first_name : `vedant`
+    }
+}
+}
+console.log(obj1);
