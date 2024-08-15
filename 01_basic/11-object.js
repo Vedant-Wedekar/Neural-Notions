93e5a2cdd894c9bf05a9d075aa2de706f9aa5f56
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-08-15 04:38:08 +0000

    remaining part of obj

--- 01_basic/11-object.js
+++ 01_basic/11-object.js
@@ -20,6 +20,7 @@ const jsuser = {
     //symbol 
 
 }
+
 console.log(jsuser.mysym)
 console.log(jsuser[mysym])
 console.log(typeof jsuser.mysym)
@@ -27,4 +28,18 @@ console.log(typeof jsuser[mysym])
 console.log(jsuser.age)
 console.log(jsuser["email"])
 
+////////////////////////////
+// this code is from different machine
+const func = function(){
+    console.log("hey this is ved")
+}
+const func2 = {
+name : "vedant"
+}
+func2 = function(){
+    console.log(`hey this is ved ${this.name}`)
+}
 
+console.log(func)
+console.log(func())
+console.log(func2())
