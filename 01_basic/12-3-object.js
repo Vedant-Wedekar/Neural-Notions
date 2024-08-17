66420cf645588533da20bb1bb7379462cfcdc8c2
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-08-17 09:25:22 +0000

    just learn about object printing , json and api data

---
+++ 01_basic/12-3-object.js
@@ -0,0 +1,29 @@
+const bca = 
+{
+    course : `bca`,
+    instrustor : `harshita maam `,
+    instrustor :`suman sirjiiiii`
+
+}
+
+// this is use for printing particular element of any object value with also use to change the 
+//namer of that entity 
+const {instrustor : guruji} = bca 
+console.log(guruji ) ; 
+
+
+
+//json code is equa;l to data present in api 
+{
+////////
+//////////
+//////////
+}
+//json code is equa;l to data present in api 
+// in array form 
+[
+    {},
+    {},
+    {},
+    {}
+]
