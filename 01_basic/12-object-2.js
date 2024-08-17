83f90cb52402bc266420e0ee2e5e3887f494fc05
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-08-17 09:23:02 +0000

    some updates in files 12-object-2.js

--- 01_basic/12-object-2.js
+++ 01_basic/12-object-2.js
@@ -19,3 +19,104 @@ const obj1 = {
 }
 }
 console.log(obj1);
+
+const poke   = {
+    1:`vedant`,
+    2:`wedekar`
+}
+
+const pokie = {1:`vihan`,2:`sharma`}
+
+const sigma  = Object.assign({},poke,pokie)
+console.log(sigma)
+const pom = { ...poke , ...pokie}
+
+console.log(pom)
+
+
+const arr1 = [
+    {
+        name:`vedant`,
+        age :19
+
+    },
+    {
+        name:`vedant`,
+        age :19
+
+    },
+    {
+        name:`vedant`,
+        age :19
+
+    },
+    {
+        name:`vedant`,
+        age :19
+
+    },
+    {
+        name:`vedant`,
+        age :19
+
+    },
+    {
+        name:`vedant`,
+        age :19
+
+    },
+    {
+        name:`vedant`,
+        age :19
+
+    },
+    {
+        name:`vedant`,
+        age :19
+
+    }
+]
+
+
+console.log(Object.keys(pokie));
+
+console.log(Object.values(pokie));
+
+console.log(Object.entries (pokie));
+
+
+console.log(pokie.hasOwnProperty('islogged'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
