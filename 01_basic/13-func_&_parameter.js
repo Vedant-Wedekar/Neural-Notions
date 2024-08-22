c7157d59a2951c709c7a9c416479e6bdd8b1aed1
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-08-22 23:38:52 +0000

    function

---
+++ 01_basic/13-func_&_parameter.js
@@ -0,0 +1,96 @@
+function say (){
+    console.log("v")
+    console.log("e")
+    console.log("d")
+    console.log("a")
+    console.log("n")
+    console.log("t")
+    console.log("")
+    console.log("")
+
+}
+say()
+
+function  add(num1,num2){
+    num1+num2
+    console.log(num1+num2)
+    return num1-num2;
+    return num1*num2;
+    
+}
+add(5,9)
+
+//num1,num2 is parameter
+
+//argument (5,9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
