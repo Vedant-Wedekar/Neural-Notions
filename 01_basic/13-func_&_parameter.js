717a09cb61dd0acac3283e46e11211f4dc84962d
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-08-24 09:26:02 +0000

    parameter

--- 01_basic/13-func_&_parameter.js
+++ 01_basic/13-func_&_parameter.js
@@ -1,19 +1,19 @@
 function say (){
-    console.log("v")
-    console.log("e")
-    console.log("d")
-    console.log("a")
-    console.log("n")
-    console.log("t")
-    console.log("")
-    console.log("")
+    // console.log("v")
+    // console.log("e")
+    // console.log("d")
+    // console.log("a")
+    // console.log("n")
+    // console.log("t")
+    // console.log("")
+    // console.log("")
 
 }
 say()
 
 function  add(num1,num2){
     num1+num2
-    console.log(num1+num2)
+    // console.log(num1+num2)
     return num1-num2;
     return num1*num2;
     
@@ -26,8 +26,28 @@ add(5,9)
 
 
 
+function login (name){
+    return `${name} is logged in sucessfully`
+}
 
+// console.log(login('vedant'))
 
+//now we are gonna to create program which print statement with added name 
+function prog (name)
+{
+    if(name === undefined || name === 'ved')
+    {
+        console.log('i guess yor the sir ved or any other person ')
+    }
+    else{
+        return  `${name} is here babyyyyy!`
+    }
+}
+
+console.log(prog('radhika'));
+    
+
+// -----------------------------------------------------------------------------
 
 
 
