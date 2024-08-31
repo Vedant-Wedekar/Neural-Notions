8b9cd126746e4e3249e6fad08b669cc319fec4a2
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-08-31 10:13:29 +0000

    for 9 min lect / comment

---
+++ 01_basic/14-varieble.js
@@ -0,0 +1,24 @@
+if(true){
+    let a = 10
+    const b =20 
+    var c = 30 
+    }
+    
+    
+    //in this case just c gone print not a abd b 
+    
+    0
+    function one(){
+        const name=`vedant
+        `
+        function two (){
+            const website =`netflex `
+            console.log(name)
+        }
+    
+        // console.log(website)
+    
+        two()
+    }
+    one()
+    
