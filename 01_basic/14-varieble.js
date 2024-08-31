17e37f0cd81398e890e06b7b1381e60b8a41ac2f
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-08-31 13:12:35 +0000

    js changes

--- 01_basic/14-varieble.js
+++ 01_basic/14-varieble.js
@@ -3,18 +3,13 @@ if(true){
     const b =20 
     var c = 30 
     }
-    
-    
     //in this case just c gone print not a abd b 
-
-    
-    
     function one(){
         const name=`vedant
         `
         function two (){
             const website =`netflex `
-            console.log(name)
+            // console.log(name)
         }
     
         // console.log(website)
@@ -22,21 +17,23 @@ if(true){
         two()
     }
     one()
-
-
-
     if(true){
         const user_name = "vedant"
 
         if(user_name){
             const name = ""
         }
-
     }
-    console.log(user_name)
+    // console.log(user_name)
 
 
+const addone = function (num)
+{
+    return num + 1
+}
 
+addone(5)
+console.log()
 
 
 
