9b654c3413f3e423a98cf756bd9577797323fad6
VEDANT WEDEKAR <vedantwedekar@gmail.com>
2024-09-29 00:41:23 +0000

    switch case

---
+++ 02_basic/18-switch.js
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+                        //switch
+
+                        const month = 3;
+                        switch(month){
+                            case 1 :
+                                console.log("jan")
+                                break;
+
+                            case 3:
+                                console.log("march")
+                                break;
+                                
+                                default:
+                                    console.log("nthing bitch ")
+                                    break ; 
+                        }
